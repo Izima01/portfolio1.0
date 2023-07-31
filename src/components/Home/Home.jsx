5e1601dcf97b42742cc5e0c70bd90531bdea6690
izima01 <kingsleyizima@gmail.com>
2023-07-31 12:28:16 +0100

    changed images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <section id="home" className="pt-8 sm:pt-16 px-0 pb-8 section">
         <div className="max-w-[968px] px-8 lg:px-0 mx-auto grid gap-6 gap-y-16 ">
-            <div className="grid lg:gap-x-8 gap-x-5 pt-14 md:pt-16 items-center home_content">
+            <div className="grid lg:gap-x-8 gap-x-5 pt-6 sm:pt-14 md:pt-16 items-center home_content">
                 <Social />
                 <div className="home_img bg-no-repeat h-52 sm:h-64 lg:h-80 w-52 sm:w-64 lg:w-80 order-[initial] md:order-1 md:justify-self-center" />
                 <Data />
--- src/components/Skills/Frontend.jsx
+++ src/components/Skills/Frontend.jsx
@@ -9,19 +9,19 @@ const skills = [
     {name: 'Bootstrap', icon: <SiBootstrap className='text-[#7310f6] text-5xl' /> },
     {name: 'Tailwind', icon: <BiLogoTailwindCss className='text-[#38bdf8] text-5xl' /> },
     {name: 'Chakra UI', icon: <SiChakraui className='text-[#319795] text-5xl' /> },
+    {name: 'Git', icon: <BiLogoGithub className='text-black text-5xl' /> },
     {name: 'Javascript', icon: <BiLogoJavascript stroke='black' className='text-[#f1dc4f] text-5xl' /> },
     {name: 'React', icon: <BiLogoReact className='text-[#149fca] text-5xl' /> },
     {name: 'Redux', icon: <BiLogoRedux className='text-[#764abd] text-5xl' /> },
-    {name: 'Git', icon: <BiLogoGithub className='text-black text-5xl' /> },
     {name: 'Firebase', icon: <img src={firebase} className='w-12' /> },
 ];
 
 const Frontend = () => {
   return (
     <div className="skills__content ">
-        <div className="skills__group flex flex-wrap gap-y-7 justify-between sm:justify-center sm:gap-7 mt-6 w-full sm:w-10/12 mx-auto">
+        <div className="skills__group flex flex-wrap justify-center gap-6 mt-6 w-full sm:w-10/12 mx-auto">
             {skills.map(({name, icon}, i) => (
-                <div key={i} className="skills__data flex items-center justify-center flex-col gap-2 loading-card shadow-md py-3 px-8 rounded-xl">
+                <div key={i} className="skills__data flex items-center justify-center flex-col gap-2 loading-card shadow-md py-3 px-8 w-auto sm:px-8 rounded-xl">
                 {/* <BiLogoHtml5 className=' text-5xl text-slate-600' /> */}
                 {icon}
                 <h3 className="skill_name text-lg leading-5 font-semibold">{name}</h3>
