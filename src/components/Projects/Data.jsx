b5466851aba1a6e31808adef9ec75134f1bac848
izima01 <kingsleyizima@gmail.com>
2023-09-01 15:17:19 +0100

    trivia app added to projects

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -3,10 +3,20 @@ import work2 from '../../assets/work2.jpg';
 import work3 from '../../assets/work3.jpg';
 import work4 from '../../assets/work4.jpg';
 import work5 from '../../assets/work5.jpg';
+import work6 from '../../assets/work6.png';
 
 export const projectsData = [
     {
         id: 1,
+        image: work4,
+        title: 'Music Player Appp',
+        stack: ['React', 'TailwindCSS','Axios', 'React Context', ],
+        description: 'A music palyer app developed with React. I consumed a custom API and used it to display the songs and albums. You can play, pause, shuffle. Also like songs and view the liked songs',
+        github: 'https://www.github.com/Izima01/Musica',
+        live: 'https://izzycodes-musica.netlify.app/'
+    },
+    {
+        id: 2,
         image: work3,
         title: 'Rest Countries API',
         stack: ['HTML', 'CSS', 'JavaScript', 'API'],
@@ -15,7 +25,7 @@ export const projectsData = [
         live: 'https://artsy-marketplace.netlify.app/'
     },
     {
-        id: 2,
+        id: 3,
         image: work1,
         title: 'Gaming Multi Step Form',
         stack: ['React', 'TailwindCSS', 'React-Switch' ],
@@ -24,22 +34,13 @@ export const projectsData = [
         live: 'https://fe-mentor-multi-step-form.netlify.app/'
     },
     {
-        id: 3,
+        id: 4,
         image: work2,
         title: 'Crypto website landing page',
         stack: ['React', 'TailwindCSS', 'Framer Motion'],
         description: 'A landing page for a cryptocurrency currency. One of my first projects on React',
         github: 'https://www.github.com/Izima01/Crappo',
         live: 'https://crappo-challengeizzy.netlify.app/'
-    },
-    {
-        id: 4,
-        image: work4,
-        title: 'Music Player Appp',
-        stack: ['React', 'TailwindCSS','Axios', 'React Context', ],
-        description: 'A music palyer app developed with React. I consumed a custom API and used it to display the songs and albums. You can play, pause, shuffle. Also like songs and view the liked songs',
-        github: 'https://www.github.com/Izima01/Musica',
-        live: 'https://izzycodes-musica.netlify.app/'
     },
     {
         id: 5,
@@ -49,5 +50,14 @@ export const projectsData = [
         description: 'An E-commerce app for NFTs where the data is gotten from Firebase. In the marketplace, you can sort and filter the products according to price, category e.t.c. You can add to cart and view your cart',
         github: 'https://www.github.com/Izima01/artsy-marketplace',
         live: 'https://artsy-marketplace.netlify.app/'
+    },
+    {
+        id: 6,
+        image: work6,
+        title: 'Trivia App',
+        stack: ['React', 'Redux Toolkit', 'React Swiper', 'Open Trivia API'],
+        description: 'A quiz app with API gottr=en from an API which has different categories and difficulties of trivia questions. Countdown timer and scoring system',
+        github: 'https://github.com/Izima01/portfolio1.0',
+        live: 'https://izzys-trivia-app.netlify.app'
     },
 ];
