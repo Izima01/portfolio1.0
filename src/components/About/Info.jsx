db6ce2f01bc862ff8dfa77c93c7fccd1f34e166f
izima01 <kingsleyizima@gmail.com>
2023-09-27 09:58:14 +0100

    tweaked animations 2

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -12,7 +12,7 @@ const Info = () => {
         <span className="about_subtitle text-sm">1+ years</span>
       </motion.div>
 
-      <motion.div initial={{ x: 160, opacity: 0 }} whileInView={{ x:0, opacity: 1 }} transition={{ duration: 1 }} className="about_box bg-white border border-[#0000001a] rounded-xl flex flex-col items-center px-2 lg:px-5 py-3 lg:py-4">
+      <motion.div initial={{ x: 100, opacity: 0 }} whileInView={{ x:0, opacity: 1 }} transition={{ duration: 1 }} className="about_box bg-white border border-[#0000001a] rounded-xl flex flex-col items-center px-2 lg:px-5 py-3 lg:py-4">
         <BiBriefcase className='about_icon text-2xl text-[#333333] mb-2' />
         <h3 className="about_title font-medium">Completed</h3>
         <span className="about_subtitle text-sm">20 + Projects</span>
