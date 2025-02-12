d454b878b4b4a59e7647d303c1ce8d12f46161a6
Izima01 <kingsleyizima@gmail.com>
2025-02-12 21:01:04 +0100

    New resume. Added Travaye to experience

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,29 +1,40 @@
 import '../../App.css';
-import CV from '../../assets/Resume-Obisike-Izima.pdf';
+import CV from '../../assets/Izima Obisike resume.pdf';
 import Info from './Info';
 import file from '../../assets/files.svg';
 import './About.css';
 
 const About = () => {
   return (
-    <div className="about section max-w-[968px] px-8 lg:px-0 mx-auto" id="about">
-        <h2 className="section__title">About Me</h2>
-        <span className="section__subtitle">My Introduction</span>
+    <div
+      className='about section max-w-[968px] px-8 lg:px-0 mx-auto'
+      id='about'
+    >
+      <h2 className='section__title'>About Me</h2>
+      <span className='section__subtitle'>My Introduction</span>
 
-        <div className="about_container grid items-center sm:gap-x-12 gap-y-10">
-            {/* <motion.img fetchpriority="true" width={356} height={356} initial={{ rotate: 270, x:-300, opacity: 0, borderRadius: '100%' }} animate={{ rotate: [240, 160, 70, 0], x:[-300, -200, -10, 0], opacity: [0, 0.3, 0.7, 1], borderRadius: ['100%', '70%', '30%', '24px'] }} transition={{ delay: 0.5, duration: 2 }} src={aboutImg}  alt="" className='about_Img rounded-3xl justify-self-center' /> */}
-              <Info />
-            <div className='about_data'>
-                <p className='about_desc p-0 mb-6 lg:mb-8'>Frontend developer, I create web pages with UI / UX user interface, I have years of experience and many clients are happy with the projects carried out.</p>
-                
-                <a href={CV} download='' className="button button--flex inline-flex gap-2">
-                    Download CV
-                    <img loading="lazy" src={file} alt="" />
-                </a>
-            </div>
+      <div className='about_container grid items-center sm:gap-x-12 gap-y-10'>
+        {/* <motion.img fetchpriority="true" width={356} height={356} initial={{ rotate: 270, x:-300, opacity: 0, borderRadius: '100%' }} animate={{ rotate: [240, 160, 70, 0], x:[-300, -200, -10, 0], opacity: [0, 0.3, 0.7, 1], borderRadius: ['100%', '70%', '30%', '24px'] }} transition={{ delay: 0.5, duration: 2 }} src={aboutImg}  alt="" className='about_Img rounded-3xl justify-self-center' /> */}
+        <Info />
+        <div className='about_data'>
+          <p className='about_desc p-0 mb-6 lg:mb-8'>
+            Frontend developer, I create web pages with UI / UX user interface,
+            I have years of experience and many clients are happy with the
+            projects carried out.
+          </p>
+
+          <a
+            href={CV}
+            download=''
+            className='button button--flex inline-flex gap-2'
+          >
+            Download CV
+            <img loading='lazy' src={file} alt='' />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -1,123 +1,129 @@
 import work1 from '../../assets/work1.webp';
-import work2 from '../../assets/work2.webp';
-import work3 from '../../assets/work3.webp';
 import work4 from '../../assets/work4.webp';
 import work5 from '../../assets/work5.webp';
 import work6 from '../../assets/work6.webp';
-import work7 from '../../assets/work7.webp';
 import work8 from '../../assets/work8.webp';
 import work9 from '../../assets/work9.webp';
 import work10 from '../../assets/work10.png';
-// import work11 from '../../assets/work 11.png';
-// import work12 from '../../assets/work 12.png';
+import work12 from '../../assets/work 12.png';
 
 export const projectsData = [
-    // {
-    //     id: 12,
-    //     image: work12,
-    //     title: 'Travaye Web App',
-    //     stack: ['React', 'Tailwind', 'MongoDB', "Express", "NodeJS", "Notification API"],
-    //     description: 'A web app to browse, save, book outing locations in your area based on different categories and budgets',
-    //     github: '',
-    //     live: 'https://www.travaye.ng/'
-    // },
-    {
-        id: 11,
-        image: work10,
-        title: 'RenCostume Web App',
-        stack: ['React', 'Zustand', 'SupaBase'],
-        description: 'An e-commerce web app to rent and buy costumes for acting or parties.',
-        github: 'https://github.com/Razeetech/RENCOSTUME',
-        live: 'https://www.rencostume.com/'
-    },
-    {
-        id: 8,
-        image: work8,
-        title: 'Talkative Chat App',
-        stack: ['React', 'TailwindCSS', 'Zustand', 'TypeScript', 'MongoDB', 'Express JS', 'Node JS', 'Socket.io'],
-        description: 'A full stack chat app where you can chat personally. You can create group chats, add or remove people on it and chat. Real-time notifications with SocketIO and fully responsive',
-        github: 'https://github.com/Izima01/talkative-chat/',
-        live: 'https://talkative-chat.vercel.app/'
-    },
-    {
-        id: 9,
-        image: work9,
-        title: 'E-Learning App',
-        stack: ['React', 'TailwindCSS', 'UseContext', 'MongoDB', 'Express JS', 'Node JS'],
-        description: 'A full stack E-Learning app where you can find E-books, VIdeos and full on courses to learn and upskill.',
-        github: '',
-        live: 'https://mellow-longma-801e80.netlify.app/'
-    },
-    {
-        id: 5,
-        image: work5,
-        title: 'NFT E-Commerce App',
-        stack: ['React', 'TailwindCSS', 'Redux Toolkit', 'Framer Motion', 'Firebase'],
-        description: 'An E-commerce app for NFTs In the marketplace, you can sort and filter the products and add to cart',
-        github: 'https://www.github.com/Izima01/artsy-marketplace',
-        live: 'https://artsy-marketplace.netlify.app/'
-    },
-    {
-        id: 13,
-        image: work6,
-        title: 'Trivia App',
-        stack: ['React', 'Redux Toolkit', 'React Swiper', 'Open Trivia API'],
-        description: 'A quiz app with API gottr=en from an API which has different categories and difficulties of trivia questions. Countdown timer and scoring system',
-        github: 'https://github.com/Izima01/portfolio1.0',
-        live: 'https://izzys-trivia-app.netlify.app'
-    },
-    // {
-    //     id: 12,
-    //     image: work11,
-    //     title: 'Grosvenor Services Website',
-    //     stack: ['React', 'Tailwind'],
-    //     description: 'A website for a company built in less a week',
-    //     github: '',
-    //     live: 'https://grosvenorservices.onrender.com/'
-    // },
-    {
-        id: 1,
-        image: work4,
-        title: 'Music Player App',
-        stack: ['React', 'TailwindCSS','Axios', 'React Context', 'Rest API'],
-        description: 'A music palyer app developed with React. I consumed a custom API and used it to display the songs and albums. You can play, pause, shuffle. Also like songs and view the liked songs',
-        github: 'https://www.github.com/Izima01/Musica',
-        live: 'https://izzycodes-musica.netlify.app/'
-    },
-    {
-        id: 2,
-        image: work3,
-        title: 'Rest Countries API',
-        stack: ['HTML', 'CSS', 'JavaScript', 'Rest API'],
-        description: 'A web app of all the countries of the world. I consumed Restcountries API and sorted and filtered the countries. You can open a details page for each country and open details for the border countries on the details page',
-        github: 'https://www.github.com/Izima01/Rest-Countries-API',
-        live: 'https://artsy-marketplace.netlify.app/'
-    },
-    {
-        id: 3,
-        image: work1,
-        title: 'Gaming Multi Step Form',
-        stack: ['React', 'TailwindCSS', 'React-Switch' ],
-        description: 'A Frontend Mentor Challenge. A form for a gaming program with different steps and options for different time frames and add-ons',
-        github: 'https://github.com/Izima01/multi-step-form',
-        live: 'https://fe-mentor-multi-step-form.netlify.app/'
-    },
-    {
-        id: 7,
-        image: work7,
-        title: 'MovieBox site',
-        stack: ['React', 'TailwindCSS', 'Axios'],
-        description: 'As site that shows differet top movies and the different details about them',
-        github: 'https://github.com/Izima01/moviebox',
-        live: 'https://hng-moviebox.netlify.app/'
-    },
-    {
-        id: 4,
-        image: work2,
-        title: 'Crypto website landing page',
-        stack: ['React', 'TailwindCSS', 'Framer Motion'],
-        description: 'A landing page for a cryptocurrency currency. One of my first projects on React',
-        github: 'https://www.github.com/Izima01/Crappo',
-        live: 'https://crappo-challengeizzy.netlify.app/'
-    },
+  {
+    id: 1,
+    category: 'fullstack',
+    image: work12,
+    title: 'Travaye Web App',
+    stack: [
+      'React',
+      'Tailwind',
+      'MongoDB',
+      'Express',
+      'NodeJS',
+      'Notification API',
+    ],
+    description:
+      'A web app to browse, save, book outing locations in your area based on different categories and budgets',
+    github: '',
+    live: 'https://www.travaye.ng/',
+  },
+  {
+    id: 2,
+    category: 'fullstack',
+    image: work10,
+    title: 'RenCostume Web App',
+    stack: ['React', 'Zustand', 'SupaBase'],
+    description:
+      'An e-commerce web app to rent and buy costumes for acting or parties.',
+    github: 'https://github.com/Razeetech/RENCOSTUME',
+    live: 'https://www.rencostume.com/',
+  },
+  {
+    id: 4,
+    category: 'fullstack',
+    image: work8,
+    title: 'Talkative Chat App',
+    stack: [
+      'React',
+      'TailwindCSS',
+      'Zustand',
+      'TypeScript',
+      'MongoDB',
+      'Express JS',
+      'Node JS',
+      'Socket.io',
+    ],
+    description:
+      'A full stack chat app where you can chat personally. You can create group chats, add or remove people on it and chat. Real-time notifications with SocketIO and fully responsive',
+    github: 'https://github.com/Izima01/talkative-chat/',
+    live: 'https://talkative-chat.vercel.app/',
+  },
+  {
+    id: 5,
+    category: 'fullstack',
+    image: work9,
+    title: 'E-Learning App',
+    stack: [
+      'React',
+      'TailwindCSS',
+      'UseContext',
+      'MongoDB',
+      'Express JS',
+      'Node JS',
+    ],
+    description:
+      'A full stack E-Learning app where you can find E-books, VIdeos and full on courses to learn and upskill.',
+
+    github: '',
+    live: 'https://mellow-longma-801e80.netlify.app/',
+  },
+  {
+    id: 6,
+    category: 'frontend',
+    image: work5,
+    title: 'NFT E-Commerce App',
+    stack: [
+      'React',
+      'TailwindCSS',
+      'Redux Toolkit',
+      'Framer Motion',
+      'Firebase',
+    ],
+    description:
+      'An E-commerce app for NFTs In the marketplace, you can sort and filter the products and add to cart',
+    github: 'https://www.github.com/Izima01/artsy-marketplace',
+    live: 'https://artsy-marketplace.netlify.app/',
+  },
+  {
+    id: 7,
+    category: 'frontend',
+    image: work6,
+    title: 'Trivia App',
+    stack: ['React', 'Redux Toolkit', 'React Swiper', 'Open Trivia API'],
+    description:
+      'A quiz app with API gottr=en from an API which has different categories and difficulties of trivia questions. Countdown timer and scoring system',
+    github: 'https://github.com/Izima01/portfolio1.0',
+    live: 'https://izzys-trivia-app.netlify.app',
+  },
+  {
+    id: 8,
+    category: 'frontend',
+    image: work4,
+    title: 'Music Player App',
+    stack: ['React', 'TailwindCSS', 'Axios', 'React Context', 'Rest API'],
+    description:
+      'A music palyer app developed with React. I consumed a custom API and used it to display the songs and albums. You can play, pause, shuffle. Also like songs and view the liked songs',
+    github: 'https://www.github.com/Izima01/Musica',
+    live: 'https://izzycodes-musica.netlify.app/',
+  },
+  {
+    id: 9,
+    category: 'frontend',
+    image: work1,
+    title: 'Gaming Multi Step Form',
+    stack: ['React', 'TailwindCSS', 'React-Switch'],
+    description:
+      'A Frontend Mentor Challenge. A form for a gaming program with different steps and options for different time frames and add-ons',
+    github: 'https://github.com/Izima01/multi-step-form',
+    live: 'https://fe-mentor-multi-step-form.netlify.app/',
+  },
 ];
